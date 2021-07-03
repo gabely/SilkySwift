c9449fa32929145f46530492df6ca52896950abe
Elenita Manicad <elenitamanicad@apples-MacBook-Air.local>
2021-07-03 17:36:12 +0400

    First commit.

---
+++ exercise3.py
@@ -0,0 +1,29 @@
+
+# Input variables
+
+coffeeOptions = ['Latte', 'Americano', 'Cappuccino', 'Espresso', 'Turkish']
+milkOptions = ['Dairy', 'Coconut', 'Almond', 'Hazeulnut','Oat','Soy']
+sizes = ['Tall','Grande','Venti']
+theOrder = []
+
+# Input display
+
+print("Let's start your day with a cofee ")
+
+for option in coffeeOptions:
+    print(option)
+
+selection = input("Please enter the coffee of your choice ")
+
+for option in milkOptions:
+    print(option)
+
+selection = input("Enter the milk of your choice ")
+
+for option in sizes:
+    print(option)
+
+selection = input("Enter your coffee size ")
+print(option)
+
+
